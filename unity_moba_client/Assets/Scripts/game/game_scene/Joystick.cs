b4753ab89a3ea942a98db02490542616fbaf5a69
FacelessXcy <xuchenyu990419@foxmail.com>
2020-05-04 19:07:57 +0800

    游戏场景UI以及遥感制作

---
+++ unity_moba_client/Assets/Scripts/game/game_scene/Joystick.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Joystick : MonoBehaviour
+{
+    public Canvas canvas;
+    private float _maxRadius=70;
+    private Transform _stick;
+
+    private Vector2 _touchDir;
+    public Vector2 TouchDir => _touchDir;
+
+    private void Start()
+    {
+        this._stick = transform.Find("stick");
+        this._touchDir = Vector2.zero;
+        this._stick.localPosition = Vector2.zero;
+    }
+
+    public void OnStickDrag(BaseEventData baseEventData)
+    {
+        Vector2 pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(this
+                .transform as RectTransform, Input.mousePosition,
+            this.canvas.worldCamera, out pos);
+        float len = pos.magnitude;
+        this._touchDir = pos.normalized;
+        if (len>_maxRadius)
+        {
+            float ratio=_maxRadius/len;//短:长
+            pos*=ratio;
+        }
+        this._stick.localPosition = pos;
+    }
+
+    public void OnStickEndDrag(BaseEventData baseEventData)
+    {
+        this._stick.localPosition = Vector2.zero;
+    }
+}
